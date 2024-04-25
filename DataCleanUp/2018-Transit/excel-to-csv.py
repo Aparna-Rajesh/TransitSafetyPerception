ac1ec2d752e97ce662f6d19c77f897f20ab57d12
Aparna Rajesh <aparnarajesh2008@gmail.com>
2024-04-24 20:06:34 -0400

    adding data cleanup dir to repo

---
+++ DataCleanUp/2018-Transit/excel-to-csv.py
@@ -0,0 +1,20 @@
+#importing pandas as pd 
+import pandas as pd 
+
+
+for x in range(12):
+    name = "arrests-in-transit-report-" + str(x + 1) + "-2018"
+
+    # Read and store content 
+    # of an excel file 
+    read_file = pd.read_excel (name + ".xlsx") 
+
+    # Write the dataframe object 
+    # into csv file 
+    read_file.to_csv (name + ".csv", 
+                    index = None, 
+                    header=True) 
+        
+    # read csv file and convert 
+    # into a dataframe object 
+    df = pd.DataFrame(pd.read_csv(name + ".csv")) 
